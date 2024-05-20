42f38b37f74bfb4bccc361b70996db6641808f7b
aandrader <samuelandraderive@gmail.com>
2024-05-20 00:24:48 -0500

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,11 +7,14 @@ const googleFont = Poppins({ subsets: ["latin"], weight: ["400", "500"] });
 
 export const metadata: Metadata = {
   title: {
-    default: "Mapa Ipuc",
+    default: "Mapa Ipuc - Iglesia Pentecostal Unida de Colombia",
     template: "Mapa Ipuc %s",
   },
   description:
     "Mapa de las congregaciones, iglesias, templos o lugares de culto de la Iglesia Pentecostal Unida de Colombia ubicados en Medellín, Bello, Copacabana, Itagüí, Sabaneta, Envigado, San Antonio de Prado, La Estrella, San Cristobal. ",
+  verification: {
+    google: "ZIWk2G1TFig1rid4a1cP1CKZACSQ2ZlOci1VNbrAemM",
+  },
 };
 
 export default function RootLayout({
